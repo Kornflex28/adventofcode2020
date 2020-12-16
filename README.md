3e5e382cedccfe422e7ac4a1cb63289f432d88f6
unknown <chauvet.louis@gmail.com>
2020-12-16 19:13:27 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# adventofcode2020
+# [Advent of Code 2020](https://adventofcode.com/)
--- code/day1.py
+++ code/day1.py
@@ -1,18 +1,10 @@
 from itertools import combinations 
 
-
 if __name__ == "__main__":
-    s = 2020
-    pairs = []
-    remains={}
-    with open("inputs/day1.txt") as f :
+    with open("./inputs/day1.txt") as f :
         data = sorted([int(n) for n in f.readlines()])
-    for n in data:
-        remain = s-n
-        if remain in remains:
-            pairs.append((n,remain,n*remain))
-        remains[n]=n
-    
+    s = 2020
+    pairs = [(a,b,a*b) for a,b in list(filter(lambda l: sum(l)==s, list(combinations(data, 2)))) ]
     triples = [(a,b,c,a*b*c) for a,b,c in list(filter(lambda l: sum(l)==s, list(combinations(data, 3)))) ]
     print(pairs)
     print(triples)
