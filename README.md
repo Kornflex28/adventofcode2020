1b781b98739a37e44d1a701adb9eab384fd0e76b
Kornflex28 <chauvet.louis@gmail.com>
2020-12-18 15:32:24 +0100

    typo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# [Advent of Code 2020](https://adventofcode.com/)
+# Advent of Code 2020
+Examples of Python solution for the [Advent of Code 2020](https://adventofcode.com/) edition.
--- code/day18.py
+++ code/day18.py
@@ -1,18 +1,21 @@
 import re
 
-MUL="*"
-ADD="+"
+MUL = "*"
+ADD = "+"
 PAR_L = "("
 PAR_R = ")"
 
-def evaluate_op(operator,x,y):
-    if operator== MUL:
+
+def evaluate_op(operator, x, y):
+    if operator == MUL:
         return x*y
     if operator == ADD:
         return x+y
+
+
 def find_first_op(s):
-    start = [m.start() for m in re.finditer(f"\{PAR_L}",s)]
-    end = [m.start() for m in re.finditer(f"\{PAR_R}",s)]
+    start = [m.start() for m in re.finditer(f"\{PAR_L}", s)]
+    end = [m.start() for m in re.finditer(f"\{PAR_R}", s)]
     if len(start) > 0:
         end = end[0]
         start = max([i for i in start if i < end])
@@ -20,14 +23,16 @@ def find_first_op(s):
     else:
         return None, None
 
+
 def evaluate(expr):
         items = expr.split()
         x = int(items.pop(0))
         while len(items) > 0:
             op = items.pop(0)
             y = int(items.pop(0))
-            x = evaluate_op(op,x, y)
+            x = evaluate_op(op, x, y)
         return x
+
 
 def evaluate_line(line):
     start, end = find_first_op(line)
@@ -37,6 +42,7 @@ def evaluate_line(line):
         start, end = find_first_op(line)
     return evaluate(line)
 
+
 def evaluate_line_2(line):
     items = line.split()
     add_indices = [i for i, j in enumerate(items) if j == '+']
@@ -45,9 +51,9 @@ def evaluate_line_2(line):
         items[i+1] = items[i+1] + f"{PAR_R}"
     return eval(" ".join(items))
 
+
 if __name__ == "__main__":
     with open("inputs/day18.txt") as f:
         data = f.read().splitlines()
-    print(sum(list(map(evaluate_line,data))))
-    print(sum(list(map(evaluate_line_2,data))))
-
+    print(sum(list(map(evaluate_line, data))))
+    print(sum(list(map(evaluate_line_2, data))))
